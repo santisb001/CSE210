02a42f2058f513f1fcb80f402313008c30db9f6f
Santiago Sburlati <santiagomatiassburlati@gmail.com>
2026-03-04 12:16:26 -0300

    Made adjustments to the code

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,7 +1,7 @@
 using System;
 
 class Program
-{
+{//this is a test for the set up
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
