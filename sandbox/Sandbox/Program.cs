82fd3780fd68f4cf2a5827c6a82f1fe9dd1dcd4f
Santiago Sburlati <santiagomatiassburlati@gmail.com>
2026-03-11 14:34:50 -0300

    excercise n°1 'input and output'

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -6,5 +6,22 @@ class Program
     {
         Console.WriteLine("Hello World! This is the Sandbox World of worlds!!!!!!.");
         Console.WriteLine("This is in C#.");
+
+        int number = 5;
+        number = 8;
+        number += 3;
+        number = "blue"; //This is wrong, a variable cannot be changed into a different type.
+
+        //To have a variable with a color I must declarea new variable
+        string color = "blue";
+        Console.WriteLine(color);
+
+        Console.Write("What color do you like? ");
+        string color = Console.Readline();
+        Console.WriteLine($"The color you like is {color}");
+
+        
+
+
     }
 }
