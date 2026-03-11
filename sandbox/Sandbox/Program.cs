f4644537b45d874c859504302e43f0b2aa90f56d
Santiago Sburlati <santiagomatiassburlati@gmail.com>
2026-03-11 14:32:41 -0300

    excercise n°1 'input and output'

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,6 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Sandbox World of worlds!!!!!!.");
+        Console.WriteLine("This is in C#.");
     }
 }
