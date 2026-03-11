e5b74fd8d4cd1db827e20c068d1d11e757837692
Santiago Sburlati <santiagomatiassburlati@gmail.com>
2026-03-11 17:23:26 -0300

    I finished Ex2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -7,45 +7,40 @@ class Program
         Console.Write("Type the grade % you received: ");
         string gradePercentage = Console.ReadLine();
         int grade = int.Parse(gradePercentage);
-
-        int A = 90;
-        int B = 80;
-        int C = 70;
-        int D = 60;
-        int F = 59;
-
-        if (grade >= A)
-        {
-            Console.WriteLine("You got an A");
-        }
-        else if (grade >= B)
-        {
-            Console.WriteLine("You got a B");
-        }
-        else if (grade >= C)
-        {
-            Console.WriteLine("You got a C");
-        }
-        else if (grade >= D)
-        {
-            Console.WriteLine("You got a D");
-        }
-        else
-        {
-            Console.WriteLine("You got a F");
-        }
-
-        if (grade >= C)
-        {
-            Console.WriteLine("Congratulations, You passed!");
-        }
-        else
-        {
-            Console.WriteLine("You have not passed.");
-            Console.WriteLine("Don't give up! Try again next time.");
-        }
-
         
 
+        string letter = "";
+
+        
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        if (grade >= 70)
+        {
+            Console.WriteLine($"Congratulations, You passed! Your grade is {letter}.");
+        }
+        else
+        {
+            Console.WriteLine($"You have not passed, your grade is {letter}.");
+            Console.WriteLine("Don't give up! Try again next time.");
+        }
     }
 }
