6a24edfea5ae0c2bd22b18e6fba98ae79b65b577
Santiago Sburlati <santiagomatiassburlati@gmail.com>
2026-03-17 17:45:05 -0300

    Abstraction resume -Finished-

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -12,6 +12,20 @@ class Program
         Job1._startYear = 2019;
         Job1._endYear = 2022;
         
+        Job Job2 = new Job();
+        Job2._company = "Apple";
+        job2._jobTitle = "Junior Programmer";
+        Job2._startYear = 2017;
+        job2._endYear = 2018;
+
+        Resume Resume1 = new Resume();
+        Resume1._name = "Santiago Sburlati";
+        Resume1._jobs.add(Job1);
+        Resume1._jobs.add(Job2);
+
+        Resume1.Display();
+   
     }
+
      
 }
--- week02/Resumes/Resume.cs
+++ week02/Resumes/Resume.cs
@@ -8,7 +8,13 @@ using System.Collections.Generic;
 
     public void Display()
         {
-            Console.Write($"{_name}");
-            Console.Write($"{_jobs}");
+            Console.WriteLine($"Name: {_name}");
+            Console.WriteLine($"Jobs: ");
+
+            foreach (Job j in _jobs)
+        {
+            j.Display();
+        }
+            
         }
     }
