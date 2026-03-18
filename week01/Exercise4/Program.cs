91df6d1a617ec9259bd482b348c4500b19923c4e
Santiago Sburlati <santiagomatiassburlati@gmail.com>
2026-03-18 13:01:43 -0300

    w in progress

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        int entryNumber = 0;
+        //Create a List
+        list<int> numbers = new list<int>();
+
+        while (entryNumber != 0)
+        {
+            
+            //Ask for a number
+            Console.WriteLine("Please type a number: ");
+            string response = Console.ReadLine();
+            entryNumber = int.Parse(response);
+
+            if (entryNumber == 0)
+            {
+                numbers.Add(entryNumber);
+            }
+            
+        
+        
+    
+        }
+        
+
+        //create a loop
+
+        //Add the total of the list
+
+        //Calculate the average
+
+
+
     }
 }
