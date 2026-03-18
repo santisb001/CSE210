38241c687276af11c6f74a6cf6b533a7a05b4682
Santiago Sburlati <santiagomatiassburlati@gmail.com>
2026-03-18 17:47:10 -0300

    W=! Exercise5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,55 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
+    
+    
+        static void DisplayWelcome()
+        {
+            Console.Write("Welcome to the program!");
+        }
+
+        static string PromptUserName()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+
+            return name;
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.WriteLine("Enter your favorite number: ");
+            string response = Console.ReadLine();
+            int favNum = int.Parse(response);
+
+            return favNum;
+        }
+
+        static int SquareNumber(int wholeNumber)
+        { 
+            int sqrdNumber = wholeNumber * wholeNumber;
+
+            return sqrdNumber;
+        }
+
+        static void DisplayResult(string nameOfUser, int squared)
+        {
+            Console.WriteLine($"{nameOfUser}, the square of your number is {squared}");
+
+        }
+
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int usernum = PromptUserNumber();
+        int sqrd = SquareNumber(usernum);
+        DisplayResult(userName,sqrd);
+
     }
+
 }
+
