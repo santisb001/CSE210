e4c382f2505b827cc5b509bcba1132b82afc565e
Santiago Sburlati <santiagomatiassburlati@gmail.com>
2026-03-18 12:02:45 -0300

    W01 Exercise3 -Compleated-

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        
+        
+        string wantToPlayAgain = "Yes";
+
+        //Console.Write("Enter the Magic Number: ");
+        //string response = Console.ReadLine();
+        Random rand = new Random();    
+        Console.WriteLine("Welcome to the Guess my Number Game!");
+        
+        while (wantToPlayAgain == "Yes")
+        {
+        int MagicNumber = rand.Next(1, 101);
+        int guessNumber = 0;
+        int tries = 0;
+
+        while (guessNumber != MagicNumber)
+            {
+
+            Console.Write("What is your guess? ");
+            string guess = Console.ReadLine();
+            guessNumber = int.Parse(guess);
+            tries ++;
+
+            if (guessNumber == MagicNumber)
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"Number of attempts: {tries}");
+                }
+            else if (guessNumber > MagicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+            else
+                {
+                    Console.WriteLine("Higher");
+                }
+            
+            }
+        Console.WriteLine("Would you like to play again? ");
+        wantToPlayAgain = Console.ReadLine();
+        wantToPlayAgain = char.ToUpper(wantToPlayAgain[0]) + wantToPlayAgain.Substring(1).ToLower();
+        }
     }
 }
