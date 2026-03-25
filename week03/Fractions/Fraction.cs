d79c23aa1c5f077e9bc98ad3e40eff64f2733336
Santiago Sburlati <santiagomatiassburlati@gmail.com>
2026-03-25 17:19:08 -0300

    Started the Encapsulation act

---
+++ week03/Fractions/Fraction.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+class Fraction()
+{
+    //Attributes
+    private int _top;
+    private int _bottom;
+
+    //Constructors
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+    public Fraction(int top)
+    {
+        _top = _top;
+        _bottom = 1;
+    }
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    //Getters and Setters
+    public int GetTop()
+    {
+        return _top;
+    }
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+
+    //Methods
+    public string GetFractionString()
+    {
+        sdfsdsd
+        return fddfdf
+    }
+
+    public double GetDecimalValue()
+    {
+        sdfsdfsdfsdf
+        return fsfsdsdf
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
