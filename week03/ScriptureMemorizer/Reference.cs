757c8bda1922e00a65fce56906b291f50f91be39
Santiago Sburlati <santiagomatiassburlati@gmail.com>
2026-03-26 17:58:55 -0300

    Started the Scripture memorizer

---
+++ week03/ScriptureMemorizer/Reference.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Reference
+{
+    
+
+    Console.WriteLine("gffgfgfgfg");
+
+
+
+
+
+
+}
---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+class Scripture
+{
+    //Atributes
+    private Reference _reference;
+    private List<Word> _words;
+
+    //Contructor
+    public Scripture(Reference reference, string text)
+    {
+        //_reference = new Reference();
+        //_words = new List<Word>();
+    }
+
+    //Methods
+    public
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
