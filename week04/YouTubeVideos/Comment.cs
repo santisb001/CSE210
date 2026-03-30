0d8429ee873b5acd50814713e6e5bfa98b4a2554
Santiago Sburlati <santiagomatiassburlati@gmail.com>
2026-03-30 15:02:43 -0300

    Started the YoutubeVideo assignment

---
+++ week04/YouTubeVideos/Comment.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+
+class Comment
+{
+    //Attributes
+    private string _name;
+    private string _text;
+
+    //Constructor
+    public Comment(string name, string text)
+    {
+        _name = name;
+        _text = text;
+    }
+
+    //Methods
+    public void DisplayComment()
+    {
+        //Display the comment data
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ week04/YouTubeVideos/Video.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+
+class Video
+{
+    //Attributes
+    private string _title;
+    private string _author;
+    private int _lengthInSeconds;
+    private List<Comment> _comments;
+
+    //Constructor
+    public Video(string title, string author, int length)
+    {
+        _title = title;
+        _author = author;
+        _lengthInSeconds = length;
+        _comments = new List<Comment>();
+    }
+
+    //Methods
+    public int ReturnNumOfComments(List<Comment> commentList)
+    {
+        //Count and return the number of items in the Comment list.
+        int numberOfComments = commentList.Count();
+        return numberOfComments;//int 
+    }
+
+    public void DisplayVideo()
+    {
+        //Display the video data and the comment data, by calling the display method of the Comment Class.
+        
+    }
+
+
+
+
+
+
+
+
+
+}
